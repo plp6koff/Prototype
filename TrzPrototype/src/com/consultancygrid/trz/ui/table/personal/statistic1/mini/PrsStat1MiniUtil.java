6190344c418153b89ed84d2bba5281b56c8afcff
plp6koff <a.palapeshkov@gmail.com>
2016-01-07 13:18:52 +0200

    mini statistic fixed

--- TrzPrototype/src/com/consultancygrid/trz/ui/table/personal/statistic1/mini/PrsStat1MiniUtil.java
+++ TrzPrototype/src/com/consultancygrid/trz/ui/table/personal/statistic1/mini/PrsStat1MiniUtil.java
@@ -15,11 +15,11 @@ public class PrsStat1MiniUtil {
 			String periodCode) {
 
 		Vector tableData = new Vector();
-		String queryNative = "SELECT 1,MATCHCODE, V18, V15,  V02,  V01,  V19, ZAPLATA_V_BROI, V14, CODE "
+		String queryNative = "SELECT 1,MATCHCODE, V18, V15,  V02,   V19, ZAPLATA_V_BROI, V14, CODE "
 				+ " FROM EMPLOYEE_STATS  "
 				+ " where 1=1 and ( MATCHCODE=:P_MATCHCODE or :P_MATCHCODE is null)  and ( CODE like :P_PERIODCODE||'%' or :P_PERIODCODE is null) "
 				+ " union all "
-				+ " SELECT 2,MATCHCODE, sum(V18),sum(V15),sum(V02),sum(V01), sum(V19),sum(ZAPLATA_V_BROI),sum(V14) , null"
+				+ " SELECT 2,MATCHCODE, sum(V18),sum(V15),sum(V02),sum(V19),sum(ZAPLATA_V_BROI),sum(V14) , null"
 				+ " FROM EMPLOYEE_STATS  "
 				+ " where 1=1  and ( MATCHCODE=:P_MATCHCODE or :P_MATCHCODE is null)  and ( CODE like :P_PERIODCODE||'%' or :P_PERIODCODE is null) "
 				+ " group by 2,MATCHCODE " + " order by 1,2,3 asc ";
@@ -34,30 +34,32 @@ public class PrsStat1MiniUtil {
 			// TODO select for each
 			Vector<Object> oneRow = new Vector<Object>();
 			int i = 1;
-			boolean boldRow = (null == currentRow[9] || " ".equals(currentRow[9]) || "N/A".equals(currentRow[9]));
+			boolean boldRow = (null == currentRow[8] || " ".equals(currentRow[8]) || "N/A".equals(currentRow[8]));
 			
-			while (i <= 5) {
+			while (i <= 4) {
 				setData(oneRow, currentRow, i, boldRow);
 				i ++;
 			}
-			Object avans = (currentRow[6] == null ? "0.0" : currentRow[6]);
+			
+			Object zpltBroi = (currentRow[6] == null ? "0.0" : currentRow[6]);
+			BigDecimal zpltBroiBD = null;
+			if (zpltBroi instanceof BigDecimal) {
+				zpltBroiBD = (BigDecimal) zpltBroi;
+			} else if (zpltBroi instanceof String) {
+				zpltBroiBD = BigDecimal.valueOf(Double.valueOf((String)zpltBroi));
+			}
+			setBoldData(oneRow, boldRow, zpltBroiBD.toString());
+			
+			Object avans = (currentRow[5] == null ? "0.0" : currentRow[5]);
 			BigDecimal avansBD = null;
 			if (avans instanceof BigDecimal) {
 				avansBD = (BigDecimal) avans;
 			} else if (avans instanceof String) {
 				avansBD = BigDecimal.valueOf(Double.valueOf((String)avans));
 			}
-			
 			setBoldData(oneRow, boldRow, avansBD.toString());
-			Object zaplt = (currentRow[7] == null ? "0.0" : currentRow[7]);
-			BigDecimal zapltBD = null;
-			if (zaplt instanceof BigDecimal) {
-			  zapltBD = (BigDecimal) zaplt;
-			} else if (zaplt instanceof String) {
-				zapltBD = BigDecimal.valueOf(Double.valueOf((String)zaplt));
-			}
 			
-			Object vaucher = (currentRow[8] == null ? "0.0" : currentRow[8]);
+			Object vaucher = (currentRow[7] == null ? "0.0" : currentRow[7]);
 			BigDecimal vaucherBD = null;
 			if (vaucher instanceof BigDecimal) {
 				vaucherBD = (BigDecimal) vaucher;
@@ -65,7 +67,7 @@ public class PrsStat1MiniUtil {
 				vaucherBD = BigDecimal.valueOf(Double.valueOf((String)vaucher));
 			}
 			
-			setBoldData(oneRow, boldRow, zapltBD.subtract(avansBD).subtract(vaucherBD));
+			setBoldData(oneRow, boldRow, zpltBroiBD.subtract(avansBD).subtract(vaucherBD));
 			for (int j = 0 ; j < oneRow.size(); j ++) {
 				System.err.print(oneRow.get(j));
 				System.out.print(":");
