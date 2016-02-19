e2b77dd7eda7033beb827ad41c894fc1ee940d78
Krasimir Dimitrov <krasimir.dimitrov@gmail.com>
2016-02-19 17:27:35 +0200

    total will be 1 line

--- TrzPrototype/src/com/consultancygrid/trz/ui/table/personal/statistic1/mini/PrsStat1MiniUtil.java
+++ TrzPrototype/src/com/consultancygrid/trz/ui/table/personal/statistic1/mini/PrsStat1MiniUtil.java
@@ -19,10 +19,10 @@ public class PrsStat1MiniUtil {
 				+ " FROM EMPLOYEE_STATS  "
 				+ " where 1=1 and ( MATCHCODE=:P_MATCHCODE or :P_MATCHCODE is null)  and ( CODE like :P_PERIODCODE||'%' or :P_PERIODCODE is null) "
 				+ " union all "
-				+ " SELECT 2,MATCHCODE, sum(V18),sum(V15),sum(V02),sum(V19),sum(ZAPLATA_V_BROI),sum(V14) , null"
+				+ " SELECT 2,'n.a.', sum(V18),sum(V15),sum(V02),sum(V19),sum(ZAPLATA_V_BROI),sum(V14) , null"
 				+ " FROM EMPLOYEE_STATS  "
 				+ " where 1=1  and ( MATCHCODE=:P_MATCHCODE or :P_MATCHCODE is null)  and ( CODE like :P_PERIODCODE||'%' or :P_PERIODCODE is null) "
-				+ " group by 2,MATCHCODE " + " order by 1,2,3 asc ";
+				+ " group by 2 " + " order by 1,2,3 asc ";
 		Query q = em.createNativeQuery(queryNative);
 		q.setParameter("P_PERIODCODE", periodCode);
 		q.setParameter("P_MATCHCODE", "");
